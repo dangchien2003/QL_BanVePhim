cf8b9f6914ccebadc484f4f4ee035096ec730dbc
dangchien2003 <chienboy03@gmail.com>
2024-06-05 02:59:29 +0700

    create hash

---
+++ src/util/encryption/hash.py
@@ -0,0 +1,19 @@
+import hashlib
+import os
+import hmac
+import base64
+
+
+class hash:
+    def getHash(string, salt=None):
+        if salt is None:
+            salt = os.urandom(16)
+        dk = hashlib.pbkdf2_hmac("sha256", string.encode(), salt, 100000)
+        return base64.b64encode(salt + dk).decode("utf-8")
+
+    def verify(hash, string):
+        decoded_data = base64.b64decode(hash)
+        salt = decoded_data[:16]
+        stored_dk = decoded_data[16:]
+        new_dk = hashlib.pbkdf2_hmac("sha256", string.encode(), salt, 100000)
+        return hmac.compare_digest(stored_dk, new_dk)
