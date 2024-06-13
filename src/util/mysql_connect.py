ca8c1bd93155925ffaf3fe5a69879b0dd90429ab
dangchien2003 <chienboy03@gmail.com>
2024-06-13 23:36:43 +0700

    add connect mysql local

--- src/util/mysql_connect.py
+++ src/util/mysql_connect.py
@@ -1,17 +1,32 @@
 import mysql.connector
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+)
 import bean.load_env
 from mysql.connector import Error
 
-class MysqlConnect :
-    def __init__(self) :
-        self.host=os.getenv("DB_HOST")
-        self.user=os.getenv("DB_USER")
-        self.password=os.getenv("DB_PASSWORD")
-        self.database=os.getenv("DB_DATABASE")
-        self.port=os.getenv("DB_PORT")
+
+class MysqlConnect:
+    def __init__(self):
+        active = os.getenv("ACTIVE").strip().lower()
+        if active == "dev":
+            self.host = os.getenv("DB_HOST")
+            self.user = os.getenv("DB_USER")
+            self.password = os.getenv("DB_PASSWORD")
+            self.database = os.getenv("DB_DATABASE")
+            self.port = os.getenv("DB_PORT")
+        elif active == "prod":
+            self.host = os.getenv("DB_HOST_PRODUCT")
+            self.user = os.getenv("DB_USER_PRODUCT")
+            self.password = os.getenv("DB_PASSWORD_PRODUCT")
+            self.database = os.getenv("DB_DATABASE_PRODUCT")
+            self.port = os.getenv("DB_PORT_PRODUCT")
+        else:
+            print("Môi trường không xác định")
+            exit()
 
     def create_connection(self):
         connection = None
@@ -21,7 +36,7 @@ class MysqlConnect :
                 user=self.user,
                 password=self.password,
                 database=self.database,
-                port=self.port
+                port=self.port,
             )
         except Error as e:
             print(f"Lỗi kết nối")
